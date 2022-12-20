00f160ef9d29b174d048c9b6af45fa4393cd95f7
ekene12e <ekenestanekeson@gmail.com>
2022-12-20 12:15:34 +0100

    fixed loacal environment

---
+++ 0x00-ES6_basic/test.js
@@ -0,0 +1,42 @@
+
+/*let a = (name) => console.log(`hello ${name}`);
+a('Ekene');
+export function taskFirst() {
+    var task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    var combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
+  console.log(taskFirst());
+  */
+  /*function f(...args) {
+    if (args.length == 0)
+    {
+        args = 'Nil';
+        return `lenght = 0, contents = ${args}`;
+    }
+    return `lenght = ${args.length}, contents = ${args}`;
+  }
+  console.log(f(2, 1, 6, 8, 99));
+  console.log(f());*/
+
+  export default function getBudgetObject(income, gdp, capita) {
+    const budget = {
+      income,
+      gdp,
+      capita,
+    };
+  
+    return budget;
+  }
+
+  console.log(getBudgetObject(400, 700, 900));
