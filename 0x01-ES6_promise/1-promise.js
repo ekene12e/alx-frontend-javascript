21ff1feac2518526b421a97f733b8df460287122
Malachy Ekene Ezema <52065233+ekene12e@users.noreply.github.com>
2022-12-20 22:27:41 +0100

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,14 @@
-export default function getFullResponseFromAPI(success) {
-	return new Promise(function(resolve, reject) {
-		if (success) {
-			resolve({
-				status : 200,
-				body: "Success"
-			});
-		} else {
-			reject(new Error("The fake API is not working currently"));
-		}
-	});
+export default function (boolean) {
+  return new Promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (boolean === true) {
+      resolve(object);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
 }
