eeaaf8d1bb5a66d135aea04c0df41c1b7b4ad8cd
ekene12e <ekenestanekeson@gmail.com>
2022-12-21 10:48:36 +0100

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from "./utils.js";
 
-function handleProfileSignup() {
+export function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((res)=>{
     let firstName = res[1].firstName;
     let lastName = res[1].lastName;
