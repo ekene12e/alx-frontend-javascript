0d4d225b2510116f11dbb264288c55d917c5dfbd
ekene12e <ekenestanekeson@gmail.com>
2022-12-21 10:46:27 +0100

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from "./utils.js";
+
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then((res)=>{
+    let firstName = res[1].firstName;
+    let lastName = res[1].lastName;
+    let body = res[0].body;
+    console.log(`${body} ${firstName} ${lastName}`);
+  })
+}
