701bc60435eaa6953ae6776b9d05b555f085b051
ekene12e <ekenestanekeson@gmail.com>
2022-12-24 16:13:02 +0100

    task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,6 @@
-import ClassRoom from './0-classroom';
+import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
+console.log(initializeRooms());
