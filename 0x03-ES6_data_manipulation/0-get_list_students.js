bd73f56233a6d725020580670cce4c7dedae47ef
ekene12e <ekenestanekeson@gmail.com>
2023-01-02 22:25:10 +0100

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+const getListStudents = () => {
+  const students = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 3,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    }
+  ];
+    return students;
+}
+export default getListStudents;
